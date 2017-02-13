9bd38a80e27c6a5c192b2b409ebae3b33fb4d019
rajrin@yahoo.com <raj@acloudfan.com>
2017-02-13 09:41:26 -0500

    new files

---
+++ sol/sample.sol
@@ -0,0 +1,1 @@
+pragma solidity ^0.4.5; contract MyContract { uint   num;event NumberSetEvent(address indexed caller, uint oldNum, uint newNum);function getNum()  returns (uint n) {return num;} function setNum(uint n) {uint old = num;num=n;NumberSetEvent(msg.sender,old,num);}function MyContract(uint x){num=x;}}
