485c290dfc04dc5dbd93a8c0e97e87cd22784822
acloudfan <rajeev@Rajeevs-MacBook-Pro.local>
2017-09-24 10:05:31 -0400

    added a script

---
+++ scripts/isSyncing.js
@@ -0,0 +1,31 @@
+/**
+ * Script for demonstrating the isSync functionality
+ * 
+ * To Test:
+ * 1) Launch geth
+ * 2) Attach to geth
+ * 3) Copy paste the code below in attached console
+ * 
+ * Repeat
+ * 4) Kill geth & start
+ * 5) In attached console you would see change in SYNCHING messages
+ */
+
+web3.eth.isSyncing(function(error, sync){
+    if(!error) {
+        // sync = boolean OR object
+        if(sync === true) {
+           // true receieved
+           console.log("SYNCHING: true")
+        } else if(sync) {
+           // object received
+           console.log("SYNCHING: ", sync.currentBlock);
+        } else {
+            // false received
+            console("SYNCHING: false")
+        }
+    } else {
+        console.log("SYNCHING: ERROR");
+        console.log(error);
+    }
+});
