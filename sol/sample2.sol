1a3421174142a9d40e78533911c5e4e2b0c6fda9
acloudfan <raj@acloudfan.com>
2018-03-25 07:28:51 -0400

    Update sample2.sol

--- sol/sample2.sol
+++ sol/sample2.sol
@@ -6,21 +6,23 @@ contract MyContract {
   event NumberSetEvent(address indexed caller, uint oldNum, uint newNum);
   event NumberDoubled(address indexed caller,uint oldNum, uint newNum);
 
-   function getNum()  returns (uint) {
+   function getNum() public returns (uint) {
      return num;
   }
                
-   function setNum(uint newNum) {
+   function setNum(uint newNum) public {
       uint old = num;
       num=newNum;
       NumberSetEvent(msg.sender,old,num);
    }
 
-   function setDouble() {
+   function setDouble() public {
       uint old = num;
       num=n;
       NumberDoubled(msg.sender,old,num);
    }
                
-   function MyContract(uint x){num=x;}
+   function MyContract(uint x) public {
+      num=x;
+   }
 }
